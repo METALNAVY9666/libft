b11d887327114964c95cef1ea0c351651ca4b3dd
Firas Zaazaa <fzaazaa@paul-f3Ar6s6.clusters.42paris.fr>
2024-11-10 15:00:06 +0100

    strdup

---
+++ srcs/ft_strdup.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 14:35:21 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/10 14:59:41 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*new;
+
+	i = 0;
+	new = (char *)malloc(sizeof(char) * ft_strlen(s));
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = 0;
+	return (new);
+}
+
+/*#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char **argv)
+{
+	char    *ft_new;
+	char    *new;
+
+	if (argc != 2)
+		return (1);
+	new = strdup(argv[1]);
+	ft_new = ft_strdup(argv[1]);
+	printf("strdup(\"%s\") -> \"%s\"\n", argv[1], new);
+	printf("ft_strdup(\"%s\") -> \"%s\"\n", argv[1], ft_new);
+	return (0);
+}*/
