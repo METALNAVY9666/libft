6a8bb896f37ff817299ee9504ce4a15f158333d8
Firas Zaazaa <fzaazaa@paul-f3Ar6s6.clusters.42paris.fr>
2024-11-10 05:51:29 +0100

    ft_memmove and updated ft_memcpy

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -1,30 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 05:14:46 by fzaazaa           #+#    #+#             */
-/*   Updated: 2024/11/10 05:48:55 by fzaazaa          ###   ########.fr       */
+/*   Created: 2024/11/10 05:34:37 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/10 05:50:49 by fzaazaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// voir issue github
+#include<stdlib.h>
 
-void	*ft_memcpy(void *dest, const void *src, unsigned int n)
+void	*ft_memmove(void *dest, const void *src, unsigned int n)
 {
 	unsigned int	i;
+	char			*tmp;
 
 	i = 0;
-	while (i++ < n)
-		*((char *)dest++) = *((char *)src++);
+	tmp = (char *)malloc(n);
+	while (i < n)
+		tmp[i++] = *((char *)src++);
+	i = 0;
+	while (i < n)
+		*((char *)dest++) = tmp[i++];
+	free(tmp);
 	return (dest);
 }
 
-/*
-#include<stdio.h>
-#include<stdlib.h>
+/*#include<stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -37,7 +41,7 @@ int	main(int argc, char **argv)
 		return (1);
 
 	text = (char *)malloc(sizeof(char) * 64);
-	ft_memcpy(text, argv[1], 5);
+	ft_memmove(text, argv[1], 5);
 	printf("%s\n", text);
 	free(text);
 
@@ -47,7 +51,7 @@ int	main(int argc, char **argv)
 	printf("->");
 	i = 0;
 	tab_dest = (int *)malloc(sizeof(int) * 3);
-	ft_memcpy(tab_dest, tab_src, sizeof(int) * 3);
+	ft_memmove(tab_dest, tab_src, sizeof(int) * 3);
 	while (i < 3)
 		printf("%i, ", tab_dest[i++]);
 	printf("\n");
