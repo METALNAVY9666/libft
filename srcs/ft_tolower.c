a47d206aae71b464efee8aaa985cb2d43026fe81
Firas Zaazaa <fzaazaa@paul-f3Ar6s6.clusters.42paris.fr>
2024-11-10 08:49:34 +0100

    ft_toupper + enhanced ft_toupper (better tests)

--- srcs/ft_tolower.c
+++ srcs/ft_tolower.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 08:30:39 by fzaazaa           #+#    #+#             */
-/*   Updated: 2024/11/10 08:48:53 by fzaazaa          ###   ########.fr       */
+/*   Created: 2024/11/10 08:42:39 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/10 08:48:43 by fzaazaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_islower(int c)
+int	ft_isupper(int c)
 {
-	return (c > 96 && c < 123);
+	return (c > 64 && c < 91);
 }
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
-	if (ft_islower(c))
-		return (c - 32);
+	if (ft_isupper(c))
+		return (c + 32);
 	return (c);
 }
 
@@ -28,16 +28,16 @@ int	ft_toupper(int c)
 int	main(void)
 {
 	int	i;
-	char text[] = "0123456789<=>?@[\\]abcdefghijklmnopqrstuvwxyz~";
+	char text[] = "0123456789<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
 	printf("%s\n\n", text);
 	i = 0;
 	while (text[i])
-		printf("%c", ft_toupper(text[i++]));
+		printf("%c", ft_tolower(text[i++]));
 	printf("\n");
 	i = 0;
 	while (text[i])
-		printf("%c", toupper(text[i++]));
+		printf("%c", tolower(text[i++]));
 	printf("\n");
 	return (0);
 }*/
