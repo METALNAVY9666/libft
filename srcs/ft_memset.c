fce025598d2907629221e8d15ef9c57847d78e05
Firas Zaazaa <fzaazaa@paul-f3Ar6s6.clusters.42paris.fr>
2024-11-10 04:52:34 +0100

    memset done

---
+++ srcs/ft_memset.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 02:55:35 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/10 04:52:01 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memset(void *s, int c, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (s);
+	while (i++ < n)
+		*((unsigned char *)s++) = (unsigned char)c;
+	return (s);
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+    int array [] = {54, 85, 20, 63, 21};
+    unsigned int size = sizeof(int) * 5;
+    unsigned int i;
+
+	i = 0;
+    while (i < 5)
+        printf("%d ", array[i++]);
+    printf("\n");
+
+    // memset(array, 2, size);
+	ft_memset(array, 0, size);
+
+	i = 0;
+    while (i < 5)
+        printf("%d ", array[i++]);
+    printf( "\n" );
+    
+    return (0);
+}*/
