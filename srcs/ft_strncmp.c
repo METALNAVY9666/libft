cf112ce80c881b3ec519c9229df10538853f3025
Firas Zaazaa <fzaazaa@paul-f3Ar6s6.clusters.42paris.fr>
2024-11-10 09:38:07 +0100

    ft_strcmp

---
+++ srcs/ft_strncmp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 13:10:39 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/10 09:36:53 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	i = 0;
+	while (s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+#include<stdio.h>
+#include<string.h>
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		return (1);
+	printf("ft_strncmp -> %i\n", ft_strncmp(argv[1], argv[2], 3));
+	printf("strncmp -> %i\n", strncmp(argv[1], argv[2], 3));
+	return (0);
+}
