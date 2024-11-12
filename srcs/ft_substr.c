816698468133c21ecea8cd9bf01873716893dc65
Firas Zaazaa <fzaazaa@paul-f3Ar3s7.clusters.42paris.fr>
2024-11-12 18:03:42 +0100

    ft_substr

---
+++ srcs/ft_substr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 17:36:33 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/12 18:03:23 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int		i;
+	int		true_len;
+	char	*sub;
+
+	i = 0;
+	true_len = 0;
+	while (s[true_len] && true_len < (int)len)
+		true_len++;
+	sub = (char *)malloc(sizeof(char) * (true_len + 1));
+	if (!sub)
+	{
+		free(sub);
+		return (0);
+	}
+	while (i < true_len)
+	{
+		sub[i] = s[i + start];
+		i++;
+	}
+	return (sub);
+}
+
+/*#include<stdio.h>
+
+int	main(void)
+{
+	char	*new;
+	
+	new = ft_substr("hello", 3, 123);
+	printf("%s\n", new);
+	free(new);
+	return (0);
+}*/
