2599ae053002d102934ed57d3a8930f2dbe17aa9
Firas Zaazaa <fzaazaa@made-f0Ar9s3.clusters.42paris.fr>
2024-11-17 13:34:21 +0100

    ft_putchar_fd OK

---
+++ srcs/ft_putchar_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 13:20:30 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/17 13:33:57 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+/*#include<fcntl.h>
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("test.txt", O_WRONLY);
+	if (fd == -1)
+		return (1);
+	ft_putchar_fd('c', fd);
+	close(fd);
+	return (0);
+}*/
