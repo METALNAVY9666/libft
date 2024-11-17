141c28117d81eb4a75e340abcc569d90755b5c20
Firas Zaazaa <fzaazaa@made-f0Ar9s3.clusters.42paris.fr>
2024-11-17 17:03:07 +0100

    reutilisantion ft_split piscine

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -5,70 +5,146 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 19:34:31 by fzaazaa           #+#    #+#             */
-/*   Updated: 2024/11/12 19:53:54 by fzaazaa          ###   ########.fr       */
+/*   Created: 2024/11/17 16:48:21 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/17 17:02:08 by fzaazaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *str)
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct s_data
+{
+	int	i;
+	int	j;
+	int	loc;
+	int	word;
+}	t_data;
+
+int	is_delimiter(char c, char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i])
-		i++;
-	return (i);
+	{
+		if (c == str[i++])
+			return (1);
+	}
+	return (0);
 }
 
-int	count_words(const char *str, char c)
+int	count_words(char *str, char *charset)
 {
 	int	i;
 	int	count;
-	int	lst;
+	int	word;
 
 	i = 0;
 	count = 0;
-	lst = 0;
+	word = 0;
 	while (str[i])
 	{
-		if ((lst && str[i] != c) || !i)
-		{
-			lst = 0;
-			count++;
-		}
+		if (is_delimiter(str[i], charset))
+			word = 0;
 		else
 		{
-			if (!lst && str[i] == c)
-				lst = 1;
+			if (!word)
+			{
+				count++;
+				word = 1;
+			}
 		}
 		i++;
 	}
 	return (count);
 }
-/*
-int	*get_positions(const char *str, char c)
+
+char	*ft_strndup(char *str, t_data *data)
 {
-	int	last;
-	int	pos;
-	int	*positions;
+	int		i;
+	int		length;
+	int		start;
+	int		end;
+	char	*res;
 
-	last = 0;
-	while (*str)
-	{
-		if (*str == c && !last)
-
-	}
+	if (!(data->word))
+		return (0);
+	i = 0;
+	length = data->i - data->j + 1;
+	start = data->j;
+	end = data->i;
+	res = (char *) malloc(sizeof(char) * (length + 1));
+	if (!res)
+		return (0);
+	while (start < end)
+		res[i++] = str[start++];
+	res[i] = 0;
+	return (res);
 }
 
-int	*get_lengths(const char *str, char c);
-*/
+int	add_text(char *str, char *charset, char **tab, t_data *data)
+{
+	if (data->word)
+	{
+		tab[data->loc] = ft_strndup(str, data);
+		if (tab[data->loc] == 0)
+			return (0);
+		data->word = 0;
+		data->loc = data->loc + 1;
+		if (data->loc == count_words(str, charset))
+			tab[data->loc] = 0;
+	}
+	return (1);
+}
 
-#include<stdio.h>
+char	**ft_split(char *str, char *charset)
+{
+	t_data		data;
+	char		**tab;
+
+	data.loc = 0;
+	data.i = 0;
+	data.j = 0;
+	data.word = 0;
+	tab = (char **) malloc(sizeof(char *) * (count_words(str, charset) + 1));
+	while (str[data.i])
+	{
+		if (is_delimiter(str[data.i], charset))
+		{
+			if (!add_text(str, charset, tab, &data))
+				return (0);
+		}
+		if (!is_delimiter(str[data.i], charset) && !data.word)
+		{
+			data.word = 1;
+			data.j = data.i;
+		}
+		data.i++;
+	}
+	if (!add_text(str, charset, tab, &data))
+		return (0);
+	return (tab);
+}
+
+void	print_str_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		printf("%i : %s\n", i, tab[i]);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
-		return (1);
-	printf("Word count : %i\n", count_words(argv[1], argv[2][0]));
+		return (0);
+	printf("Occurences : %i\n", count_words(argv[1], argv[2]));
+	printf("Splits :\n");
+	print_str_tab(ft_split(argv[1], argv[2]));
 	return (0);
 }
