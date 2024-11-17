953690c236e45c7d9635f694d27d9b21285829a7
Firas Zaazaa <fzaazaa@made-f0Ar9s3.clusters.42paris.fr>
2024-11-17 14:53:16 +0100

    ft_putnbr_fd OK

---
+++ srcs/ft_putnbr_fd.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzaazaa <fzaazaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 14:45:24 by fzaazaa           #+#    #+#             */
+/*   Updated: 2024/11/17 14:52:49 by fzaazaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	int	tmp;
+
+	tmp = n;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -tmp;
+		ft_putchar_fd('-', fd);
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
+}
+
+/*#include<fcntl.h>
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("test.txt", O_WRONLY);
+	if (fd == -1)
+		return (1);
+	ft_putnbr_fd(-2147483648, fd);
+	close(fd);
+	return (0);
+}*/
